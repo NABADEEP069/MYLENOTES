b50ef378cfabe0a3c9dcb742d179b716ff00db49
Debanand Singha <debanand1101@gmail.com>
2024-07-30 19:23:19 +0530

    mobile view

--- src/components/notes/notes.tsx
+++ src/components/notes/notes.tsx
@@ -48,7 +48,7 @@ const popularNotes = [
 
 export default function Notes() {
   return (
-    <div className="grid sm:grid-cols-2 md:grid-cols-4 gap-5 mt-4">
+    <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-4">
       {popularNotes.map((note, index) => (
         <div
           key={index}
